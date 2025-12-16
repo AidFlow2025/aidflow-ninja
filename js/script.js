b2d4a3891a0879726ce731094d9ba45b8c848f78
AidFlow2025 <aidflow2.0@gmail.com>
2025-12-16 08:09:56 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+﻿const toggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+// cerrar menú al tocar un link
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('active');
+  });
+});
